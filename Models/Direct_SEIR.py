924e09b638e115604c37b0cbdff58647854cc8d6
eliminated123 <bo24469@bristol.ac.uk>
2026-04-24 16:35:57 +0100

    Plot function added to visualise the changes in population of each agent over a range of 100 days

--- Models/Direct_SEIR.py
+++ Models/Direct_SEIR.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 class SEIRModel:
     
     def __init__(self):
@@ -10,28 +12,41 @@ class SEIRModel:
         self.__inf_list = [0]
         self.__rec_list = [0]
 
-        def __update_sus (self, sus, inf):
-            return sus - self.exposure_rate * inf * sus
+    def __update_sus (self, sus, inf):
+        return sus - self.exposure_rate * inf * sus
 
-        def __update_exp (self, exp, sus, inf):
-            return exp + self.exposure_rate * inf * sus - self.incubation_rate * exp 
+    def __update_exp (self, exp, sus, inf):
+        return exp + self.exposure_rate * inf * sus - self.incubation_rate * exp 
 
-        def __update_inf (self, inf, exp):
-            return inf + self.incubation_rate * exp - self.recovery_rate * inf
+    def __update_inf (self, inf, exp):
+        return inf + self.incubation_rate * exp - self.recovery_rate * inf
 
-        def __update_rec (self, rec, inf):
-            return rec + self.recovery_rate * inf
+    def __update_rec (self, rec, inf):
+        return rec + self.recovery_rate * inf
 
-        def calculate(self):
-            for i in range(0, 100):
-                sus = self.__sus_list[-1]
-                exp = self.__exp_list[-1]
-                inf = self.__inf_list[-1]
-                rec = self.__rec_list[-1]
-                
-                self.__sus_list.append(self.__update_sus(sus, inf))
-                self.__exp_list.append(self.__update_exp(exp, sus, inf))
-                self.__inf_list.append(self.__update_inf(inf, exp))
-                self.__rec_list.append(self.__update_rec(rec, inf))
+    def calculate(self):
+        for i in range(0, 100):
+            sus = self.__sus_list[-1]
+            exp = self.__exp_list[-1]
+            inf = self.__inf_list[-1]
+            rec = self.__rec_list[-1]
+            
+            self.__sus_list.append(self.__update_sus(sus, inf))
+            self.__exp_list.append(self.__update_exp(exp, sus, inf))
+            self.__inf_list.append(self.__update_inf(inf, exp))
+            self.__rec_list.append(self.__update_rec(rec, inf))
 
-                
+    def plot(self):
+        fig, ax = plt.subplots()
+        plt.plot(self.__days_list, self.__sus_list, label = "Susceptible")
+        plt.plot(self.__days_list, self.__exp_list, label = "Exposed")
+        plt.plot(self.__days_list, self.__inf_list, label = "Infected")
+        plt.plot(self.__days_list, self.__rec_list, label = "Recovered")
+        ax.set_ylabel("Fraction of population")
+        ax.set_xlabel("Days")
+        plt.legend()
+        plt.show()
+
+model = SEIRModel()
+model.calculate()
+model.plot()
