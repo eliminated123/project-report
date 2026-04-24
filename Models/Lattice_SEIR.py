76c5375d79048ad0a79d549059e0255d6284e7c7
eliminated123 <bo24469@bristol.ac.uk>
2026-04-24 17:36:43 +0100

    lattice intialised and the movement of agents added

--- Models/Lattice_SEIR.py
+++ Models/Lattice_SEIR.py
@@ -1,2 +1,75 @@
-class lattice:
-    pass
+import numpy as np
+from numpy import random
+
+class Lattice:
+    def __init__(self, size = 100, seed = 1234, sigma = 0.1, gamma = 0.005): ##initialising variables 
+        self.size = size
+        self.grid = np.zeros((size,size), dtype=int) ## creates grid using arrays of size x size with only vacant sites represented by 0
+        self.rng = np.random.default_rng(seed)
+        
+        self.sigma = sigma
+        self.gamma = gamma
+
+        self.sus_pop = [] ##empty list to store population of each agent
+        self.exp_pop = []
+        self.inf_pop = []
+        self.rec_pop = []
+    
+    def initialise_grid(self, total_particles = 250, ratio_exp = 0.05): 
+        init_exp = int(ratio_exp * total_particles) ##calculates number of intial exposed agents
+        init_sus = int(total_particles * (1-ratio_exp)) ##calculates number of initial susceptible agents
+
+        total_cells = self.size * self.size ##total number of cells
+        
+        chosen_index= self.rng.choice(total_cells, total_particles, replace = False) ##randomly selects total particle numbers from 0 to total cells, with no duplicates
+
+        coordinates = np.unravel_index(chosen_index, (self.size,self.size)) ## converts indices to grid positions (co-ordinates)
+        
+        self.grid[coordinates[0][:init_exp], coordinates[1][:init_exp]] = 2 ##takes first exposed positions and changes grid co-ordinates to 2 to represent exposed agents
+        self.grid[coordinates[0][init_exp:], coordinates[1][init_exp:]] = 1 ##takes remaining positions and changes grid co-ordinates to 1 to represent susceptible agents
+        
+        return(self.grid) ##returns updated grid
+
+    def get_neighbours(self, i , j):  
+        neighbours = [] ##empty list to store the neighbouring cells of a co-ordinate
+        if i > 0:
+            neighbours.append((i - 1, j))
+        if i < self.size - 1:
+            neighbours.append((i + 1, j))
+        if j > 0:
+            neighbours.append((i, j - 1))
+        if j < self.size - 1:
+            neighbours.append((i, j + 1)) ## finds and stores cells above and next to the co-ordinate (parameters chosen so the grid is modelled as a hard wall)
+
+        if i > 0 and j > 0:
+            neighbours.append((i - 1, j - 1))
+        if i > 0 and j < self.size - 1:
+            neighbours.append((i - 1, j + 1))
+        if i < self.size - 1 and j > 0:
+            neighbours.append((i + 1, j - 1))
+        if i < self.size -1  and j < self.size - 1 :
+            neighbours.append((i + 1, j + 1)) ##find and stores diagonal cells
+
+
+        return neighbours ##returns list of neighbours
+
+
+    def move_agents(self):
+        agents = np.argwhere(self.grid !=0) ##identifies the non-vacant sites in grid
+        self.rng.shuffle(agents) ##randomly reorganises agents in array
+
+        for i, j in agents:
+            if self.grid[i,j] == 0:
+                continue ##identifies if the agent has already moved by checking if co-ordinate is empty
+            
+            state = self.grid[i, j] ##records value (agent) in cell 
+            neighbours = self.get_neighbours(i, j) ##find neighbours using function above
+
+            ni, nj = neighbours[self.rng.integers(len(neighbours))] ##randomises the neightbours in list and selects one
+
+            if self.grid[ni, nj] == 0: ##checks if the chosen neighbouring cell is empty
+                self.grid[ni, nj] = state ##updates chosen cell to the value of the agent (moves agent)
+                self.grid[i, j] = 0 ##updates original position of agent back to 0 (empty)
+
+    
+    
